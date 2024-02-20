be564216f8ab3807c74d4b6edef99b624b97ca7d
vortexdude <nitinnamdeo456@gmail.com>
2024-02-20 20:27:39 +0530

    fix indent

--- app/api/auth/main.py
+++ app/api/auth/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from .schema import UserCreation, UserOut
 from .service import UserService
 import pymongo
+
 router = APIRouter()
 
 
@@ -9,21 +10,21 @@ router = APIRouter()
 async def root():
     return {"User": "Nitin"}
 
+
 #  response_model=UserOut
 @router.post("/signup", summary="Create New User", response_model=UserOut)
 async def create_user(data: UserCreation):
-        user = await UserService.find_by_email(data.email)
-        if user:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="User is already present with the email"
-            )
+    user = await UserService.find_by_email(data.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User is already present with the email"
+        )
 
-        data = await UserService.create_user(data)
-        user_in = {
-            'user_id': data.user_id,
-            'user_name': data.user_name,
-            'enabled': data.enabled
-        }
-        return user_in
-
+    data = await UserService.create_user(data)
+    user_in = {
+        'user_id': data.user_id,
+        'user_name': data.user_name,
+        'enabled': data.enabled
+    }
+    return user_in
