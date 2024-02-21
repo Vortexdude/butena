ce64b5019f4d8afd2d849b287fdd3a05325051e9
vortexdude <nitinnamdeo456@gmail.com>
2024-02-21 20:22:26 +0530

    fix the file cleanup and schema

--- app/api/functionality/schema.py
+++ app/api/functionality/schema.py
@@ -1,10 +1,8 @@
 from pydantic import BaseModel
 import typing
-from uuid import uuid4 as uuid
 
 
 class Deployment(BaseModel):
-    id: typing.Optional[str]
     github_url: str
     repo_type: str
 
@@ -12,7 +10,6 @@ class Deployment(BaseModel):
         "json_schema_extra": {
             "examples": [
                 {
-                    "id": f"{uuid()}",
                     "github_url": "https://github.com/Vortexdude/static_site.git",
                     "repo_type": "static",
                 }
--- app/api/functionality/service.py
+++ app/api/functionality/service.py
@@ -1,6 +1,6 @@
 from git import Repo
-from os import path, remove
-
+import os
+import shutil
 
 class CloudOperations:
     def __init__(self, user_id, github_url, repo_type):
@@ -14,12 +14,12 @@ class CloudOperations:
         return {"Status": "Success"}
 
     def clone(self):
-        if path.isdir(self.clone_dir):
+        if os.path.isdir(self.clone_dir):
             print("clone directory already exist.")
             print(f"Removing ... {self.clone_dir}")
-            remove(self.clone_dir)
+            shutil.rmtree(self.clone_dir)
+
         try:
-            local_dir = f"/tmp/git/{self.user_id}"
-            Repo.clone_from(self.github_url, local_dir)
+            Repo.clone_from(self.github_url, self.clone_dir)
         except Exception:
             raise Exception(f"Cant able to clone the repo {self.github_url}")
