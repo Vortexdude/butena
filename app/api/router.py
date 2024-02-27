1241ed5a32bac4c0b53720911dee6695134a6c2c
vortexdude <nitinnamdeo456@gmail.com>
2024-02-27 23:07:52 +0530

    add the health endpoint for testing

--- app/api/router.py
+++ app/api/router.py
@@ -4,6 +4,12 @@ from app.api.functionality.main import router as cloud_operations
 from app.api.auth.jwt import auth_router
 
 api_router = APIRouter()
+
+@api_router.get("/health")
+def health():
+    return {"message": "You are live"}
+
+
 api_router.include_router(root_router, prefix="/users", tags=['user'])
 api_router.include_router(cloud_operations, prefix="/operations", tags=['cloud_operations'])
 api_router.include_router(auth_router, prefix="/auth", tags=['auth'])
