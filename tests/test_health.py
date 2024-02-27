97052380c672c433f519b2446e44fda02a844108
vortexdude <nitinnamdeo456@gmail.com>
2024-02-27 23:05:54 +0530

    add pytest

---
+++ tests/test_health.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.core.main import get_app
+client = TestClient(get_app())
+
+
+def test_health():
+    response = client.get("/api/health")
+    assert response.status_code == 200
+    assert response.json() == {"message": "You are live"}
