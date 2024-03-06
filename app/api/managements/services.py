723a0b2962a95bd1e58b4f8fd92e25fe9c9d95a1
vortexdude <nitinnamdeo456@gmail.com>
2024-03-06 19:49:58 +0530

    :warning: except the exceptions

--- app/api/managements/services.py
+++ app/api/managements/services.py
@@ -1,5 +1,6 @@
 from app.core.db.models import Department, Employee
 from sqlalchemy.orm import Session
+from app.exceptions import UserException, DatabaseException, StatusCode
 
 
 class BaseService:
@@ -7,22 +8,38 @@ class BaseService:
         self.db = db
 
     def save_to_db(self, data):
-        self.db.add(data)
-        self.db.commit()
-        self.db.refresh(data)
+        try:
+            self.db.add(data)
+            self.db.commit()
+            self.db.refresh(data)
+        except Exception as e:
+            self.db.rollback()
+            raise e
         return data
 
     def get_all(self, Model):
-        return self.db.query(Model).all()
+        try:
+            return self.db.query(Model).all()
+        except Exception as e:
+            raise e
 
     def get_by_id(self, Model, _id: str):
-        return self.db.query(Model).filter_by(id=_id).first()
+        try:
+            return self.db.query(Model).filter_by(id=_id).first()
+        except Exception as e:
+            raise e
 
     def get_by_email(self, Model, email: str):
-        return self.db.query(Model).filter_by(email=email).first()
+        try:
+            return self.db.query(Model).filter_by(email=email).first()
+        except Exception as e:
+            raise e
 
     def get_by_name(self, Model, name: str):
-        return self.db.query(Model).filter_by(name=name).first()
+        try:
+            return self.db.query(Model).filter_by(name=name).first()
+        except Exception as e:
+            raise e
 
 
 class DepartmentServices(BaseService):
@@ -32,7 +49,7 @@ class DepartmentServices(BaseService):
     def create(self, name: str = None):
         _dep = self.get_by_name(Model=Department, name=name)
         if _dep:
-            return {"Status": "Department is already exists"}
+            raise DatabaseException(status_code=StatusCode.ALREADY_202)
 
         dp_in = Department(name=name)
         self.db.add(dp_in)
@@ -41,7 +58,11 @@ class DepartmentServices(BaseService):
         return dp_in
 
     def get_all_departments(self):
-        return self.get_all(Model=Department)
+        departments = self.get_all(Model=Department)
+        if not departments:
+            raise DatabaseException(status_code=StatusCode.NOT_FOUND_204)
+
+        return departments
 
 
 class EmployeeServices(BaseService):
@@ -52,7 +73,7 @@ class EmployeeServices(BaseService):
         email = data['email']
         emp = self.get_by_email(Model=Employee, email=email)
         if emp:
-            return {"Status": "Employee already exist"}
+            raise UserException(status_code=StatusCode.CONFLICT_409)
 
         dp_in = Employee(**data)
         return self.save_to_db(dp_in)
@@ -60,11 +81,15 @@ class EmployeeServices(BaseService):
     def delete_record(self, **data):
         email = data['email']
         emp = self.get_by_email(Model=Employee, email=email)
-        if emp:
-            return {"Status": "Record is already exist so deleting the record"}
+        if not emp:
+            raise UserException(status_code=StatusCode.BAD_REQUEST_400)
 
     def update(self, **data):
         pass
 
     def get_all_employee(self):
-        return self.get_all(Model=Employee)
+        employees = self.get_all(Model=Employee)
+        if not employees:
+            raise DatabaseException(status_code=StatusCode.NOT_FOUND_204)
+
+        return employees
